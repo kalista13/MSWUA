289359161aeed66d21810472d46577c9073133da
Angela <angdkay@gmail.com>
2025-07-22 17:00:49 -0600

    Add sponsors

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,6 +2,10 @@ import './HomePage.css';
 import quoteText from '../assets/images/quote-text.svg';
 import LogoImage from '../assets/images/mswua-logo.svg';
 import planetImage from '../assets/images/planet-icon.svg';
+import sponsor1 from '../assets/images/sponsors/sponsor-engineer.svg';
+import sponsor2 from '../assets/images/sponsors/sponsor-sed-eeds.svg';
+import sponsor3 from '../assets/images/sponsors/sponsor-ap!rg.svg';
+import sponsor4 from '../assets/images/sponsors/sponsor-rockaboo.svg';
 
 export function HomePage () {
   return (
@@ -30,6 +34,15 @@ export function HomePage () {
           </div>
           <div className="image-holder">
             <img src={planetImage} alt="planet icon" />
+          </div>
+        </div>
+
+        <div className="sponsor-section">
+          <div className="sponsor-bg">
+            <img className="engineer-sponsor" src={sponsor1} alt="sponsor 1" />
+            <img className="seds-sponsor" src={sponsor2} alt="sponsor 2" />
+            <img className="aprg-sponsor" src={sponsor3} alt="sponsor 3" />
+            <img className="rockaboo-sponsor" src={sponsor4} alt="sponsor 4" />
           </div>
         </div>
       </section>
