d4d1bfded91040e83e399a6ebe7720e70c7772f0
Angela <angdkay@gmail.com>
2025-08-08 15:16:30 -0600

    Style the lines in projects section and add animation on explore boxes

--- eslint.config.js
+++ eslint.config.js
@@ -4,17 +4,23 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
+import react from 'eslint-plugin-react'
 
 export default tseslint.config([
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
+    plugins: {react},
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
     ],
+    rules: {
+      'react/jsx-props-no-spreading': 'off',
+    },
+    settings: { react: { version: 'detect'}},
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
--- src/pages/projectpage/ProjectPage.tsx
+++ src/pages/projectpage/ProjectPage.tsx
@@ -1,9 +1,20 @@
 import './ProjectPage.css';
+import checkpoint from '../../assets/images/icons/checkpoint-icon.png';
 export function ProjectPage () {
   return (
     <>
       <div id="projects">
-        
+        <h1>Built By Us</h1>
+        <p>Completed Projects</p>
+        <div className="line-section">
+          <div className="h-line line-1"></div>
+          <img className="checkpoint-1" src={checkpoint} alt="check point icon" />
+          <div className="h-line line-2"></div>
+          <img className="checkpoint-2" src={checkpoint} alt="check point icon" />
+          <div className="h-line line-3"></div>
+          <img className="checkpoint-3" src={checkpoint} alt="check point icon" />
+          <div className="h-line line-4"></div>
+        </div>
       </div>
     </>
   )
