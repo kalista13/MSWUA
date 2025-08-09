c39763a4464ffa3560a4aa0d24ee55ea0e8b12e5
Angela <angdkay@gmail.com>
2025-08-09 15:12:57 -0600

    Add line animation

--- src/pages/projectpage/ProjectPage.tsx
+++ src/pages/projectpage/ProjectPage.tsx
@@ -1,19 +1,51 @@
 import './ProjectPage.css';
 import checkpoint from '../../assets/images/icons/checkpoint-icon.png';
+import { motion } from "framer-motion";
+
 export function ProjectPage () {
+
+  const lineMovement = {
+    initial: {scaleY: 0},
+    whileInView: {scaleY: 1},
+    transition: { duration: 0.8},
+    viewport: { once: true, amount: 0.6, margin: "-10% 0px -10% 0px"},
+  }
+
   return (
     <>
       <div id="projects">
         <h1>Built By Us</h1>
         <p>Completed Projects</p>
+        <div className="can-rgx-v">
+          <p>2021-22</p>
+          <h1>CAN-RGX V</h1>
+          <p>The beginning of our journey</p>
+          <button>learn more</button>
+        </div>
         <div className="line-section">
-          <div className="h-line line-1"></div>
+
+          <motion.div {...lineMovement} className="h-line line-1"></motion.div>
+
           <img className="checkpoint-1" src={checkpoint} alt="check point icon" />
-          <div className="h-line line-2"></div>
+
+          <motion.div {...lineMovement}  
+            transition={{ ...lineMovement.transition, delay: 0.8 }}
+            className="h-line line-2">
+          </motion.div>
+
           <img className="checkpoint-2" src={checkpoint} alt="check point icon" />
-          <div className="h-line line-3"></div>
+
+          <motion.div {...lineMovement}  
+            transition={{ ...lineMovement.transition, delay: 0.8 }}
+            className="h-line line-3">
+          </motion.div>
+
           <img className="checkpoint-3" src={checkpoint} alt="check point icon" />
-          <div className="h-line line-4"></div>
+          
+          <motion.div {...lineMovement}  
+            transition={{ ...lineMovement.transition, delay: 0.5 }}
+            className="h-line line-4">
+          </motion.div>
         </div>
       </div>
     </>
